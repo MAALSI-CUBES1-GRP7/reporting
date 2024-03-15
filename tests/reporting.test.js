69cb0d8cfb92557f16e5071d63c9f2a94c5a07c2
Yorim NIABE <yorim.niabe@bluesoft-group.com>
2024-03-15 06:02:42 +0100

    init CI/CD

---
+++ tests/reporting.test.js
@@ -0,0 +1,9 @@
+const {} = require("expect");
+
+let server;
+
+describe("Testing Reporting API", () => {
+  it('GET /api/reporting/test should return "Reporting test"', async () => {
+    expect(200).toBe(200);
+  });
+});
