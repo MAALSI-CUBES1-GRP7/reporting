f146e5fb5a229bc4480f987f5a8fa85573d06b4a
Yorim NIABE <yorim.niabe@bluesoft-group.com>
2024-03-15 15:12:46 +0100

    modification

--- routes/reportings.js
+++ routes/reportings.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/test", async (req, res) => {
-  res.status(200).json({ message: "Reporting test" });
+  res.status(200).json({ message: "Reporting test2" });
 });
 
 module.exports = router;
